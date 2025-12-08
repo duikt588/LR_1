c8b50f7f274c9778406e1ed7637d6bea18a416d5
duikt588 <st9177817@stud.duikt.edu.ua>
2025-12-08 14:04:35 +0200

    Create 1v.cpp

---
+++ 7/1v.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int c0;
+    int steps = 0;
+    cout << "Input number: ";
+    cin >> c0;
+    
+    if (c0 <= 0) {
+        cout << "Need to be natural number" << endl;
+        return 0;
+    }
+    
+    while (c0 != 1) {
+        cout << c0 << endl;
+        
+        if (c0 % 2 == 0) {
+            c0 = c0 / 2;
+        } else {
+            c0 = 3 * c0 + 1;
+        }
+        
+
+        steps++;
+    }
+    
+    cout << c0 << endl;
+    cout << "steps = " << steps;
+    return 0;
+}
