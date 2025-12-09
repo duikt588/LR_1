0e7f5000af0ee6b0f17a06f2f1accf34c79eea8e
duikt588 <st9177817@stud.duikt.edu.ua>
2025-12-09 14:17:15 +0200

    Update 3v.cpp

--- 4/3v.cpp
+++ 4/3v.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main() {
-    int n1 = 1;
-    int n2 = 1;
+    int n1;
+    int n2;
+    cout << ("n1: ")
+    cin >> n1;
+    cout << ("n2: ")
+    cin >> n2;
     float r1 = 1.0f / n1;
     float r2 = 1.0f / n2;
     const float e = 0.000001;
