536fd0ab6dda889c7083ed6abb12889be8cfd6a2
duikt588 <st9177817@stud.duikt.edu.ua>
2025-12-15 20:46:19 +0200

    Update 2v.cpp

--- 9/2v.cpp
+++ 9/2v.cpp
@@ -7,19 +7,20 @@ using namespace std;
 int main() {
     const int n = 10;
     int a[n];
-    int i, A, count = 0;
+    int i, A;
+    int count = 0;
 
     srand(time(0));
 
     cout << "R:\n";
     for (i = 0; i < n; i++) {
-        a[i] = rand() % 41 - 20;
+        a[i] = rand() % 41 - 20; // випадкові числа [-20; 20]
         cout << a[i] << " ";
     }
     cout << endl;
 
-    A = rand() % 21;
-    cout << "A = " << A << endl;
+    cout << "A: ";
+    cin >> A;
 
     for (i = 0; i < n; i++) {
         if (a[i] > A) {
