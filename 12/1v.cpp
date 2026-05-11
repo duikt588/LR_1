fa4d73ef2f2c70411341da6ad817deb99dba8777
duikt588 <st9177817@stud.duikt.edu.ua>
2026-05-11 18:12:37 +0300

    Create 1v.cpp

---
+++ 12/1v.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+    int n = sizeof(vector) / sizeof(vector[0]);
+    int *ptr = vector;
+    int min = *ptr;
+    for (int i = 0; i < n; i++) {
+        if (*(ptr + i) < min) {
+            min = *(ptr + i);
+        }
+    }
+
+    cout << "Minimum: " << min << endl;
+    return 0;
+}
