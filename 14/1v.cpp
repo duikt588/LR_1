3ac4978287b7fe6d3d16b027fd40a31edb563b87
duikt588 <st9177817@stud.duikt.edu.ua>
2026-05-13 16:28:49 +0300

    Update 1v.cpp

--- 14/1v.cpp
+++ 14/1v.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-using namespace std;
 
+using namespace std;
 int main() {
     srand(time(0));
-    int n = 10;
+    int n;
+    cout << "Array (n): ";
+    cin >> n;
+
     float *arr = new float[n];
     int pos = 0, neg = 0;
-    cout << "Array:" << endl;
+    cout << "Elements:" << endl;
     for (int i = 0; i < n; i++) {
         arr[i] = (float(rand()) / float(RAND_MAX)) * 4.0 - 2.0;
         cout << arr[i] << "  ";
         if (arr[i] > 0) pos++;
         else if (arr[i] < 0) neg++;
     }
-    cout << "\n\nPositive: " << pos << endl;
-    cout << "Negative: " << neg << endl;
+    cout << "\n\nPositive: " << pos << "\nNegative: " << neg << endl;
     delete[] arr;
-
     return 0;
 }
