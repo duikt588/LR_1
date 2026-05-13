46819e83e21f442ab0a6d23372f928def46ff9fe
duikt588 <st9177817@stud.duikt.edu.ua>
2026-05-13 16:11:41 +0300

    Create 3v.cpp

---
+++ 15/3v.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isLeap(int year) {
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
+int monthLength(int year, int month) {
+    int lengths[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (month == 2 && isLeap(year)) {
+        return 29;
+    }
+    
+    return lengths[month];
+}
+
+int main(void) {
+    for(int yr = 2000; yr < 2002; yr++) {
+        for(int mo = 1; mo <= 12; mo++)
+            cout << monthLength(yr, mo) << " ";
+        cout << endl;
+    }
+    return 0;
+}
